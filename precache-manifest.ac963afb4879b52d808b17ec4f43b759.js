daba0d919cc52947e60800ef5dcdd0ff5f994a8d
Karlelisa <karlelisaw@gmail.com>
2020-02-09 17:19:21 -0500

    Updates

---
+++ precache-manifest.ac963afb4879b52d808b17ec4f43b759.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7db78099c8201b5b9d8a8019eadf3c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "92a5b9c714a2c86a68a8",
+    "url": "/Clicky-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d1e02548e5d629ae1aac",
+    "url": "/Clicky-Game/static/css/main.f26f7ce7.chunk.css"
+  },
+  {
+    "revision": "92a5b9c714a2c86a68a8",
+    "url": "/Clicky-Game/static/js/2.0220f4be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.0220f4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e02548e5d629ae1aac",
+    "url": "/Clicky-Game/static/js/main.ac268452.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  },
+  {
+    "revision": "c17ae3f222db02e1c357e14e1b769f2e",
+    "url": "/Clicky-Game/static/media/field-background-with-trees.c17ae3f2.jpg"
+  },
+  {
+    "revision": "bc8750fd1389936180e9b86aea819fe7",
+    "url": "/Clicky-Game/static/media/green-pokemon-logo.bc8750fd.png"
+  }
+]);
